64a441f6670d10a682ec425444c189453e4ad839
z.tetsuya.t.takano <takano_tetsuya@hand.co.jp>
2019-10-09 19:13:24 +0900

    ビルドフロー用

---
+++ Editor/Core/Pipeline/ChipstarBuildFlow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar
+{
+	public abstract class ChipstarBuildFlow : ScriptableObject
+	{
+		public abstract void Build();
+	}
+}
---
+++ Editor/Core/Pipeline/ChipstarPipeline.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar
+{
+	/// <summary>
+	/// Chipstarのビルドを管理するヤツ
+	/// </summary>
+	public sealed class ChipstarPipeline : ScriptableObject
+	{
+		[SerializeField] private ChipstarBuildFlow[] m_buildFlowList = default;
+
+		/// <summary>
+		/// ビルド実行
+		/// </summary>
+		public void Build()
+		{
+			try
+			{
+				for (var i = 0; i < m_buildFlowList.Length; i++)
+				{
+					var flow = m_buildFlowList[i];
+					using (var scope = StopWatchScope.Create(flow.name))
+					{
+						flow.Build();
+					}
+				}
+
+			}
+			catch( Exception e )
+			{
+				Debug.LogException(e);
+			}
+			finally
+			{
+				// TODO 
+			}
+		}
+	}
+}
