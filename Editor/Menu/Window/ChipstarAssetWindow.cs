ae72a45074724de1606a914b8eacf89193a48abc
z.tetsuya.t.takano <takano_tetsuya@hand.co.jp>
2019-10-15 18:28:17 +0900

    [CL]Classs一覧取得

--- Editor/Menu/Window/ChipstarAssetWindow.cs
+++ Editor/Menu/Window/ChipstarAssetWindow.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-namespace Chipstar
+namespace Chipstar.Builder.Window
 {
 	/// <summary>
 	/// 
 	/// </summary>
 	public sealed class ChipstarAssetWindow : EditorWindow
 	{
+		//====================================
+		// 変数
+		//====================================
+		private AssetClassFilter m_classFilter = default;
 
+		//====================================
+		// 関数
+		//====================================
+
+		private void OnEnable()
+		{
+			m_classFilter = new AssetClassFilter();
+
+			m_classFilter.LoadAssembly();
+		}
+
+		private void OnDisable()
+		{
+			
+		}
+
+		private void OnGUI()
+		{
+			
+		}
 	}
 }
---
+++ Editor/Menu/Window/System/AssetClassFilter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Chipstar.Builder.Window
+{
+	/// <summary>
+	/// ChipstarAssetを抽出するモノ
+	/// </summary>
+	public class AssetClassFilter
+	{
+		//==================================
+		// Field
+		//==================================
+		private List<Type> m_typeList = new List<Type>();
+
+		//==================================
+		// Method
+		//==================================
+
+		public void LoadAssembly()
+		{
+			var assembly = System.Reflection.Assembly.GetAssembly(typeof(ChipstarAsset));
+			// Assemblyから一覧を取り出す
+			var chipstars
+				= assembly.GetTypes()
+				// 抽象クラスは無視
+				.Where( c => !c.IsAbstract )
+				// アセット派生クラス
+				.Where(c => c.IsSubclassOf(typeof(ChipstarAsset)))
+				.ToArray();
+				;
+			m_typeList.AddRange( chipstars );
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public IReadOnlyList<Type> GetList( )
+		{
+			return m_typeList;
+		}
+		public IReadOnlyList<Type> GetList( Func<Type,bool> condition )
+		{
+			return m_typeList.Where(condition).ToArray();
+		}
+	}
+}
