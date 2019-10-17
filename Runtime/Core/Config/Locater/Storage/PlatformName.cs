63aa5ffceab32ef452a13271d60d31f30c3061ba
z.tetsuya.t.takano <takano_tetsuya@hand.co.jp>
2019-10-17 16:31:56 +0900

    [CL]機種チェック修正

--- Runtime/Core/Config/Locater/Storage/PlatformName.cs
+++ Runtime/Core/Config/Locater/Storage/PlatformName.cs
@@ -6,20 +6,25 @@ using UnityEngine;
 
 namespace Chipstar
 {
-    public class PlatformName : ChipstarAsset
-    {
-        [Serializable]
-        private sealed class PlatformToName
-        {
-            public RuntimePlatform Platform = default;
-            public string Name = default;
-        }
-        [SerializeField]
-        private PlatformToName[] m_table = default;
+	public class PlatformName : ChipstarAsset
+	{
+		[Serializable]
+		private sealed class PlatformToName
+		{
+			public RuntimePlatform Platform = default;
+			public string Name = default;
+		}
+		[SerializeField]
+		private PlatformToName[] m_table = default;
 
-        public string Get( RuntimePlatform platform)
-        {
-            return m_table.FirstOrDefault(c => c.Platform == platform)?.Name;
-        }
-    }
+		public string Get(RuntimePlatform platform)
+		{
+			var pl = m_table.FirstOrDefault(c => c.Platform == platform);
+			if( pl == null)
+			{
+				throw new Exception("Not Found Platform :: "+platform);
+			}
+			return pl.Name;
+		}
+	}
 }
