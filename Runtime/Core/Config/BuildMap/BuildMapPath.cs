1be757d5c59d82190a4163dc0eac03c2f6ac522f
z.tetsuya.t.takano <takano_tetsuya@hand.co.jp>
2019-10-17 12:45:34 +0900

    [CL]各Manifestの出力先を扱うクラス

---
+++ Runtime/Core/Config/BuildMap/BuildMapPath.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar.Downloads
+{
+	public class BuildMapPath : ChipstarAsset
+	{
+		//================================
+		// SerializeField
+		//================================
+		[SerializeField] private BuildMapName m_buildMapName = default;
+		[SerializeField] private StoragePath m_storagePath = default;
+
+		//================================
+		// 関数
+		//================================
+
+		public IAccessLocation Get( RuntimePlatform platform)
+		{
+			return m_storagePath.Get(platform).ToLocation(m_buildMapName.Name);
+		}
+	}
+}
