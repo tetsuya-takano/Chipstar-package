54b089b6aaa470ae22ff3cb0dcb34ae1aaaf35d0
z.tetsuya.t.takano <takano_tetsuya@hand.co.jp>
2019-10-18 11:55:40 +0900

    バージョンを書き出す

---
+++ Runtime/Manifest/Version/ManifestVersionFileBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar
+{
+	public abstract class ManifestVersionFileBuilder : FileBuilderCreater<ManifestVersion>
+	{
+
+	}
+
+	public abstract class ManifestVersionParser : FileParser<ManifestVersion>
+	{
+		public ManifestVersionParser(IFileConverter converter) : base(converter)
+		{
+		}
+	}
+	public abstract class ManifestVersionWriter : FileWriter<ManifestVersion>
+	{
+		public ManifestVersionWriter(IFileConverter converter) : base(converter)
+		{
+		}
+	}
+}
---
+++ Runtime/Manifest/Version/ManifestVersionPath.cs
@@ -0,0 +1,19 @@
+﻿using Chipstar.Downloads;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar
+{
+	public class ManifestVersionPath : ChipstarAsset
+	{
+		[SerializeField] private ServerPath m_outputPath = default;
+		[SerializeField] private ManifestVersionName m_versionName = default;
+
+		public IAccessLocation Get(RuntimePlatform platform)
+		{
+			return m_outputPath.Get(platform).ToLocation( m_versionName.Name );
+		}
+	}
+}
